fa4df519cc808b1542163e531e2a4f30462d329a
Karol Dzialowski <karlososhd@gmail.com>
2020-05-12 13:40:38 +0200

    Filtrowanie sekwencyjne obrazow

--- lab_10_filtry/filtry.cpp
+++ lab_10_filtry/filtry.cpp
@@ -7,8 +7,29 @@
 using namespace std;
 
 unsigned char*** color;
+unsigned char*** colorCopy;
 int height;
 int width;
+
+double average[9] = {
+  1.0, 1.0, 1.0,
+  1.0, 1.0, 1.0,
+  1.0, 1.0, 1.0,
+};
+
+double blur[25] = {
+  0.0, 1.0, 2.0, 1.0, 0.0,
+  1.0, 4.0, 8.0, 4.0, 1.0,
+  2.0, 8.0, 16.0, 8.0, 2.0,
+  1.0, 4.0, 8.0, 4.0, 1.0,
+  0.0, 1.0, 2.0, 1.0, 0.0,
+};
+
+double sharpen [9] = {
+  -1.0, -1.0, -1.0,
+  -1.0, 9.0, -1.0,
+  -1.0, -1.0, -1.0
+};
 
 void load_image() {
   FILE *fp;
@@ -51,7 +72,74 @@ void load_image() {
   fclose(fp);
 }
 
-void save_to_file() {
+void filter(double* mask, int maskWidth, int maskSum) {
+  colorCopy = new unsigned char**[width];
+  for (int i=0; i<width; i++) {
+    colorCopy[i] = new unsigned char*[height];
+    for (int j=0; j<height; j++) {
+      colorCopy[i][j] = new unsigned char[3];
+      for (int k=0; k<3; k++) {
+        colorCopy[i][j][k] = color[i][j][k];
+      }
+    }
+  }
+
+  int offset = maskWidth/2;
+
+  for (int i=0; i<width; i++) {
+    for (int j=0; j<height; j++) { 
+      int sum_0 = 0;
+      int sum_1 = 0;
+      int sum_2 = 0;
+
+      for (int mask_i = 0; mask_i < maskWidth; mask_i++) {
+        for (int mask_j = 0; mask_j < maskWidth; mask_j++) {
+          int image_i = i + mask_i - offset;
+          int image_j = j + mask_j - offset;   
+          if (image_i < 0 || image_j < 0 || image_i >= width || image_j >= height) {
+            sum_0 += 0;
+            sum_1 += 0;
+            sum_2 += 0; 
+          } else {
+            int multiplier = mask[mask_i * maskWidth + mask_j]; 
+            //multiplier = 1;
+            sum_0 += multiplier * color[image_i][image_j][0]; 
+            sum_1 += multiplier * color[image_i][image_j][1]; 
+            sum_2 += multiplier * color[image_i][image_j][2]; 
+          } 
+        } 
+      }
+
+      int output_0 = sum_0/maskSum;
+      int output_1 = sum_1/maskSum;
+      int output_2 = sum_2/maskSum;
+
+      if (output_0 < 0) {
+        output_0 = 0;
+      } else if (output_0 > 255) {
+        output_0 = 255;      
+      }
+
+      if (output_1 < 0) {
+        output_1 = 0;
+      } else if (output_1 > 255) {
+        output_1 = 255;      
+      }
+
+      if (output_2 < 0) {
+        output_2 = 0;
+      } else if (output_2 > 255) {
+        output_2 = 255;      
+      }
+
+      colorCopy[i][j][0] = output_0;
+      colorCopy[i][j][1] = output_1;
+      colorCopy[i][j][2] = output_2;
+    }
+  } 
+}
+
+void save_to_file(unsigned char*** image) {
   const int MaxColorComponentValue = 255;
   FILE *fp;
   const char *filename = "output.ppm";
@@ -64,7 +152,7 @@ void save_to_file() {
   {
     for (int j = 0; j < width; j++)
     {
-      fwrite(color[i][j], 1, 3, fp);
+      fwrite(image[i][j], 1, 3, fp);
     }
   }
   fclose(fp);
@@ -73,15 +161,19 @@ void save_to_file() {
   for (int i=0; i<width; i++) {
     for (int j=0; j<height; j++) {
       delete[] color[i][j];
+      delete[] colorCopy[i][j];
     }
     delete[] color[i];
+    delete[] colorCopy[i];
   }
   delete[] color;
+  delete[] colorCopy;
 }
 
 
 int main() {
   load_image();
-  save_to_file();
+  filter(sharpen, 3, 1);
+  save_to_file(colorCopy);
   return 0;
 }
